4f3195e78aacb59298631860c7e24ada768fd478
Chibinha <2180623@my.ipleiria.pt>
2021-01-16 19:29:54 +0000

    adding float button and search bar

--- app/src/main/java/com/example/azone_android/MainActivity.java
+++ app/src/main/java/com/example/azone_android/MainActivity.java
@@ -9,23 +9,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private DrawerLayout drawer;
     private FragmentManager fragmentManager;
+    NavigationView navigationView;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         drawer = findViewById(R.id.drawer);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -33,13 +36,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         navigationView.setNavigationItemSelectedListener(this);
 
-        fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment = new ProductListFragment();
+        setTitle("A+Zone");
+        fragmentManager.beginTransaction().replace(R.id.contentFragment, fragment).commit();
 
         // TODO: Show user data on header
     }
     
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/azone_android/ProductListFragment.java
@@ -0,0 +1,73 @@
+package com.example.azone_android;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProductListFragment extends Fragment {
+
+
+    public ProductListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_product_list, container, false);
+
+        // Add options menu
+        setHasOptionsMenu(true);
+        // Set fab click listener
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Add intent to open shopping cart activity
+            }
+        });
+
+        return view;
+    }
+
+    // TODO: Finish search
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        // Add menu_search as options menu
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem itemSearch = menu.findItem(R.id.item_search);
+        SearchView searchView = (SearchView) itemSearch.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
